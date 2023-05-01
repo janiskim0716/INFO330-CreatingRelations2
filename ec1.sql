a993fb6839aa32186791279c0b9512fc5f5127cb
Janis Kim <janisk2@uw.edu>
2023-04-30 22:40:07 -0700

    extra credit1

---
+++ ec1.sql
@@ -0,0 +1,21 @@
+-- Adding Huskichu pokemon 
+INSERT INTO imported_pokemon_data(abilities, against_bug, against_dark, against_dragon,
+against_electric, against_fairy, against_fight, against_fire, against_flying,
+against_ghost,against_grass, against_ground, against_ice, against_normal,
+against_poison, against_psychic, against_rock, against_steel, against_water,
+attack, base_egg_steps, base_happiness, base_total, capture_rate, classfication,
+defense, experience_growth, height_m, hp, name, percentage_male, pokedex_number,
+sp_attack, sp_defense, speed, type1, type2, weight_kg, generation, is_legendary)
+VALUES ("['Cute Charm','Run Away']",0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0, 40, 20, 20,
+20, 20, "Dog Pokemon", 40, 50, 1, 25, "Huskichu", 40, 802, 30, 30, 30, "Mascot", NULL, 18, 1,0);
+
+-- Adding Cougarite pokemon
+INSERT INTO imported_pokemon_data (abilities, against_bug, against_dark, against_dragon,
+against_electric, against_fairy, against_fight, against_fire, against_flying,
+against_ghost,against_grass, against_ground, against_ice, against_normal,
+against_poison, against_psychic, against_rock, against_steel, against_water,
+attack, base_egg_steps, base_happiness, base_total, capture_rate, classfication,
+defense, experience_growth, height_m, hp, name, percentage_male, pokedex_number,
+sp_attack, sp_defense, speed, type1, type2, weight_kg, generation, is_legendary)
+VALUES ("['Slow Attack']",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 50, 20, 20,
+20, 30, "Pokemon", 20, 50, 1, 50, "Cougarite", 20, 803, 10, 15, 10, "Mascot", NULL, 25, 1,0);
